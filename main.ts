2bf068c4ae927f62e609db5a089a00fb263fe042
gchen1996 <1328486271@qq.com>
2020-05-19 11:07:29 +0800

    basic updata

---
+++ main.ts
@@ -0,0 +1,14 @@
+//% color="#FF7F50" weight=10 icon="\uf785" block="basic"
+namespace basic{
+	
+	
+    /**
+     * Touch button
+     */
+    
+    //% blockId=touchbutton block="touch |digital pin %pin" blockExternalInputs=false  group="触摸按键"
+    //% weight=70
+    export function touchButton(pin: DigitalPin): number {
+        return pins.digitalReadPin(pin)
+    }
+}
